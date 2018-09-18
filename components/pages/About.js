d1ecd312d89f1f47824e07eb50a9bc7d90a31122
Nilkun <30467227+nilkun@users.noreply.github.com>
2018-09-19 00:57:26 +0900

    Update About.js

--- components/pages/About.js
+++ components/pages/About.js
@@ -10,7 +10,9 @@ const About = () => {
         <br>
         <br>
         I'm an avid Arch Linux user and Awesome WM fan, who speaks broken JavaScript and C++, 
-        and get things done with a bit of trial and syntax error. I am passionate about computers, and program daily in order to improve my skills and land my dream job as a developer.
+        and get things done with a bit of trial and syntax error. I am passionate about computers, 
+        and program daily in order to improve my skills and land my dream job as a developer. If you want to see some of
+        my current projects, then visit <a href="https://github.com/nilkun"> my GitHub page</a>.
         <br>
         <br>
         The first computer I ever owned was a Zenith Z-181 with two 3.5" floppy-disk drives. I spent a lot of time on it playing Space Quest and typing in BASIC games that never worked. 
